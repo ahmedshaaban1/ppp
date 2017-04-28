0e08bdedcd596b76e4ec279d4879cc136adcac59
Christopher Kotfila <chris.kotfila@kitware.com>
2017-04-28 15:59:14 -0400

    Initial commit

---
+++ fib.py
@@ -0,0 +1,22 @@
+import click
+
+def fibonacci(N):
+    if N < 2:
+        return N
+
+    return fibonacci(N - 2) + fibonacci(N - 1)
+
+@click.command()
+@click.option("-l", default=False, is_flag=True)
+@click.argument('n', type=click.INT)
+def main(n, l):
+    if l:
+        for i in range(n):
+            print("The fibonacci number for '{}' is {}".format(
+                i, fibonacci(i)))
+
+    print("The fibonacci number for '{}' is {}".format(
+        n, fibonacci(n)))
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='fib',
+      version='0.0.0',
+      description='Fibonacci',
+      long_description='Run Fibonacci calculations',
+      author='Christopher Kotfila',
+      author_email='kotfic@gmail.com',
+      url='',
+      py_modules=['fib'],
+      install_requires=[],
+      license='Apache 2.0',
+      zip_safe=False,
+      entry_points= {
+          'console_scripts': ['fib=fib:main']
+      },
+      keywords='ansible')
