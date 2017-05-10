4df04eae633397c188593c4ad93b25ff039163bd
Christopher Kotfila <chris.kotfila@kitware.com>
2017-05-10 17:15:47 -0400

    Fix bugs in bootstrap

--- bootstrap_ec2.sh
+++ bootstrap_ec2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install -y build-essential python3-dev python3-pip htop
-sudo pip3 install virtualenvwrapper
-source virtualenvwrapper.sh
+sudo apt-get update
+sudo apt-get install -y build-essential python-dev python3-dev python3-pip virtualenvwrapper htop
+
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+
 mkvirtualenv -a /opt/ppp -p python3 ppp
 pip install -r requirements.txt
 pip install -e .
