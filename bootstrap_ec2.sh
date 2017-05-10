6f7c4cdf3273eccd1f5eb197ae124930f6c2dd3e
Christopher Kotfila <chris.kotfila@kitware.com>
2017-05-10 17:21:04 -0400

    Additional Fixes

--- bootstrap_ec2.sh
+++ bootstrap_ec2.sh
@@ -4,8 +4,9 @@ sudo apt-get update
 sudo apt-get install -y build-essential python-dev python3-dev python3-pip virtualenvwrapper htop
 
 export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 
-mkvirtualenv -a /opt/ppp -p python3 ppp
+
+mkvirtualenv -a /opt/ppp -p /usr/bin/python3 ppp
 pip install -r requirements.txt
 pip install -e .
