22877e38ec6e4d75afd8dbf9c16ef6b74d901a6d
Christopher Kotfila <chris.kotfila@kitware.com>
2017-05-10 17:26:14 -0400

    Make sure we set up bashrc

--- bootstrap_ec2.sh
+++ bootstrap_ec2.sh
@@ -6,6 +6,10 @@ sudo apt-get install -y build-essential python-dev python3-dev python3-pip virtu
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 
+echo "" >> ~/.bashrc
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> ~/.bashrc
+
 
 mkvirtualenv -a /opt/ppp -p /usr/bin/python3 ppp
 pip install -r requirements.txt
